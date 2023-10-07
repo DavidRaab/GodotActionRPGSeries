068cac20c9f73a66490020b5062de1274c5c4630
David Raab <davidraab83@gmail.com>
2023-10-07 20:15:31 +0200

    Added Two TileSets & TileMaps

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,7 +6,9 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@onready var anim : AnimationPlayer = self.get_node("AnimationPlayer")
+@onready var anim     : AnimationPlayer = self.get_node("AnimationPlayer")
+@onready var animTree : AnimationTree   = $AnimationTree
+@onready var animState :AnimationNodeStateMachinePlayback = animTree.get("parameters/playback")
 
 # Direction the characters walks to
 enum Direction {Up, Right, Down, Left, IdleUp, IdleRight, IdleDown, IdleLeft}
@@ -25,10 +27,11 @@ func update_direction():
         else:
             self.direction = Direction.Left
     else:
-        if   self.direction == Direction.Up:    self.direction = Direction.IdleUp
-        elif self.direction == Direction.Right: self.direction = Direction.IdleRight
-        elif self.direction == Direction.Down:  self.direction = Direction.IdleDown
-        elif self.direction == Direction.Left:  self.direction = Direction.IdleLeft
+        match self.direction:
+            Direction.Up:    self.direction = Direction.IdleUp
+            Direction.Right: self.direction = Direction.IdleRight
+            Direction.Down:  self.direction = Direction.IdleDown
+            Direction.Left:  self.direction = Direction.IdleLeft
         
 
 func _physics_process(_delta):
@@ -37,17 +40,26 @@ func _physics_process(_delta):
     self.velocity = dir * SPEED if dir else Vector2.ZERO
     self.move_and_slide()
     
+    var normalized = self.velocity.normalized()
+    if normalized == Vector2.ZERO:
+        animState.travel("Idle")
+    else:
+        animTree.set("parameters/Idle/blend_position", normalized)
+        animTree.set("parameters/Run/blend_position", normalized)
+        animState.travel("Run")
+    
     # Update the direction
-    self.update_direction()
+#    self.update_direction()
 
     # play correct animation depending on direction
-    if   self.direction == Direction.Up       : anim.play("Up")
-    elif self.direction == Direction.Down     : anim.play("Down")
-    elif self.direction == Direction.Left     : anim.play("Left")
-    elif self.direction == Direction.Right    : anim.play("Right")
-    elif self.direction == Direction.IdleUp   : anim.play("IdleUp")
-    elif self.direction == Direction.IdleRight: anim.play("IdleRight")
-    elif self.direction == Direction.IdleDown : anim.play("IdleDown")
-    else                                      : anim.play("IdleLeft")
+#    match self.direction:
+#        Direction.Up        : anim.play("Up")
+#        Direction.Down      : anim.play("Down")
+#        Direction.Left      : anim.play("Left")
+#        Direction.Right     : anim.play("Right")
+#        Direction.IdleUp    : anim.play("IdleUp")
+#        Direction.IdleRight : anim.play("IdleRight")
+#        Direction.IdleDown  : anim.play("IdleDown")
+#        _                   : anim.play("IdleLeft")
     
     
