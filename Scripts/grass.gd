89d7415a048ed1cb5d206ee6f989273d06f1d6a1
David Raab <davidraab83@gmail.com>
2023-10-13 00:29:22 +0200

    Added Layers for Physics Collision

--- Scripts/grass.gd
+++ Scripts/grass.gd
@@ -3,9 +3,20 @@ extends Node2D
 @onready var main   : Node2D      = get_tree().current_scene
 @onready var effect : PackedScene = preload("res://Prefabs/GrassEffect.tscn")
 
-func _process(_delta):
-    if Input.is_action_just_pressed("player_attack"):
-        var obj : Node2D = effect.instantiate()
-        obj.global_position = self.global_position
-        main.add_child(obj)
-        self.queue_free()
+func destroy_me():
+    var obj : Node2D = effect.instantiate()
+    obj.global_position = self.global_position
+    main.add_child(obj)
+    self.queue_free()
+
+func _on_area_2d_area_entered(_area):
+    print("Area Entered")
+    destroy_me()
+
+func _on_area_2d_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+    print("Shape Entered")
+    destroy_me()
+
+func _on_area_2d_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+    print("Body Shape Entered")
+    destroy_me()
