474836e335361c9a7ffe5a3d1627c5fa1681eb02
David Raab <davidraab83@gmail.com>
2023-10-12 23:27:02 +0200

    Input Handling must be done in _process

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -7,9 +7,21 @@ const SPEED = 100.0
 enum State {Idle, Move, Attack}
 
 # State of the character
-var state     = State.Idle
-var direction = Direction.Left
+var state          : State          = State.Idle
+var direction      : Direction.Type = Direction.Left
+var attack_pressed : bool           = false
+var input_vector   : Vector2        = Vector2.ZERO
 
+func _process(_delta):
+    # When we are in Attack State we skip input polling
+    if self.state == State.Attack:
+        return
+    
+    if Input.is_action_just_pressed("player_attack"):
+        attack_pressed = true
+        
+    input_vector = Input.get_vector("player_left", "player_right", "player_up", "player_down")
+    
 # Changing the state can be dissallowed. For example we must wait until an attack finish
 # until we can do other things again, like movement, or attacking again.
 func state_change_allowed() -> bool:
@@ -19,14 +31,11 @@ func state_change_allowed() -> bool:
     return true
 
 func _physics_process(_delta):
-    # Get input information
-    var is_attack    : bool    = Input.is_action_just_pressed("player_attack")
-    var input_vector : Vector2 = Input.get_vector("player_left", "player_right", "player_up", "player_down")
-    
     if state_change_allowed():
         # Attack has higher priority than movement
-        if is_attack:
-            self.state = State.Attack
+        if attack_pressed:
+            self.state     = State.Attack
+            attack_pressed = false
         # Check Movement
         else:
             match Direction.from_vector2(input_vector):
